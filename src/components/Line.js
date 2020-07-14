ff5a7eeca03b7dadcc301798cf6c4488993a0290
mmacdonald1 <melmacd94@gmail.com>
2020-07-13 22:24:36 -0400

    line graph

---
+++ src/components/Line.js
@@ -0,0 +1,54 @@
+import React,{Component} from 'react'
+var Chart = require("chart.js");
+
+class Line extends Component{
+  constructor(props) {
+    super(props);
+    //grab reference value
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    //on update add to data
+    this.myChart.data.datasets = this.props.data
+    this.myChart.update();
+  }
+  componentDidMount() {
+      //Chart js line graph settings
+      this.myChart = new Chart(this.canvasRef.current, {
+      type: 'line',
+      data: {
+          datasets: this.props.data
+      },
+      options: {
+          title: {
+            display: true,
+            text: 'Number of House Plants Bought at Lowes Daily'
+          },
+          scales: {
+            xAxes: [{
+              type: 'linear',
+              position: 'bottom',
+              scaleLabel: {
+                display: true,
+                labelString: 'Number of Plants Bought A Day'
+              }
+            }],
+            yAxes: [{
+              position: 'left',
+              scaleLabel: {
+                display: true,
+                labelString: 'Frequency of Number Sold'
+              }
+            }]
+          }
+        }
+    });
+  }
+
+  render(){
+    return <canvas id="myCanvas" ref={this.canvasRef} />;
+  }
+}
+
+export default Line
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,110 @@
 import React,{Component} from 'react'
+import Scatter from './Scatter.js'
+import Line from './Line.js'
 
 class Main extends Component{
-  componentDidMount(){
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scatterData: []
+    };
+  }
+
+  componentDidMount() {
+    //on build retrieve data
     this.getData()
   }
 
   getData=()=>{
-    fetch('http://localhost:3000/plants')
+    //get data by num rows
+    fetch('http://localhost:3000/plants/200/getByNumRows')
     .then(resp => resp.json())
-    .then(console.log)
+    .then(data =>{
+      if(!data.error){
+          this.aggregateByPlant(data)
+      }
+    })
   }
+
+  aggregateByPlant=(data)=>{
+    let plantsObj={}
+
+    //for each data obj grab relevent keys, store value in array, and increment count
+    data.forEach((item, i) => {
+      for (const [key, value] of Object.entries(item)) {
+        //if key does not exist add it else add value to array
+        if(!plantsObj[key] && key !== "createdAt" && key !== "updatedAt"&& key !== "id"){
+          plantsObj[key]={}
+          plantsObj[key].arr = []
+          plantsObj[key].arr.push(value)
+        }else if(key !== "createdAt" && key !== "updatedAt"&& key !== "id"){
+          plantsObj[key].arr.push(value)
+        }
+      }
+    });
+    this.aggregateFrequency(plantsObj)
+  }
+
+  aggregateFrequency=(plantsObj)=>{
+    let plantData=[]
+    //for each plant object count frequency that each number occurs
+    for (const [key, value] of Object.entries(plantsObj)) {
+      let plantObj = {}
+      plantObj.label = key
+      let freqObj={}
+      value.arr.forEach((item, i) => {
+        if(!freqObj[item]){
+          freqObj[item] = 1
+        }else{
+          freqObj[item] = freqObj[item] +1
+        }
+      });
+      plantObj.data = freqObj
+      plantData.push(plantObj)
+    }
+    this.formatChartData(plantData)
+  }
+
+  formatChartData=(plantData)=>{
+    //format count to x, frequency to y, and add chart.js styles
+    let colors=["#c73e82","#39d7fa","#2196f3","#ffa600", "#45de02"]
+    let backColor=["#f2b8d5","#b3f1ff","#8ac1ed","#ffd380","#b5f09c"]
+    plantData.forEach((item, i) => {
+      let finalObj = {}
+      finalObj.label = item.label
+      finalObj.data=[]
+      finalObj.borderColor=colors[i]
+      finalObj.backgroundColor= backColor[i]
+      finalObj.fill = false
+      finalObj.radius = 5
+      finalObj.lineTension = .3
+
+      for (const [key, value] of Object.entries(item.data)) {
+        let obj={}
+        obj.y = value
+        obj.x = key
+        finalObj.data.push(obj)
+
+      }
+      //set state
+      this.setState({scatterData: [...this.state.scatterData, finalObj]})
+    })
+  }
+
 
   render(){
     return(
-      <div>
-        <h1>Hii</h1>
+      <div class="container">
+        <div class="header-div">
+          <img class="plant-img" src="plant.png"/>
+          <h1 class="title-text">Mel's Code Challenge</h1>
+          <img class="plant-img" src="plant.png"/>
+        </div>
+        <div className="sub chart-wrapper">
+          {/* If data then render line graph */}
+          {this.state.scatterData && this.state.scatterData[0] ? <Line data={this.state.scatterData}/>: null}
+        </div>
       </div>
     )
   }
